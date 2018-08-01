0998f5db828fba04abd9eeefa22c48231942fddf
CaballepMobile <jose.caballero@mobileappscompany.us>
2018-08-01 19:05:41 -0400

    Daily3 finished

---
+++ Week1/Training/BestProjectEver/app/src/main/java/com/example/admin/bestprojectever/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.admin.bestprojectever;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etText;
+    TextView tvLabel;
+    Button btnSubmit;
+    private Button lalala1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etText = findViewById(R.id.etText);
+        tvLabel = findViewById(R.id.tvLabel);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvLabel.setText(etText.getText().toString());
+            }
+        });
+    }
+}
